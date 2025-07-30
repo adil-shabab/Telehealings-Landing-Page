27db0f6594984fed16d0a1a0fab3c0ed911403d4
adil-shabab <adilshabab2004@gmail.com>
2025-07-30 18:40:36 +0530

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+  const swiper = new Swiper('.testimonial-swiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      0: {
+        slidesPerView: 1,
+      }
+    }
+  });
+
